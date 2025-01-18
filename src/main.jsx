b32106b0dd01a0ffdd14861dc81fd2f53d3df98c
Shervin <shervindarmankifarahani@gmail.com>
2025-01-18 03:15:19 -0500

    removed redundant call to app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import NewsInterface from './NewsInterface.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
     <NewsInterface />
   </StrictMode>,
 )
